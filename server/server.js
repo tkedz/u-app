86fc02edc2741498f1ce056bb1a6f08aa14ce6c9
Tomek <tomek@example.com>
2020-03-07 12:57:21 +0100

    start 🚀

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const app = require('./app');
+
+const port = process.env.PORT || 3333;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+const x = 23;
+console.log('kapa');
